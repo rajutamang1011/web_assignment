f1918728d27f4be2dc079cdb11ea9a8b07b3a991
rajutamang1011 <rajutamang1011@gmail.com>
2024-01-14 15:33:01 +0545

    SCSS setup

--- src/pages/public/Home.jsx
+++ src/pages/public/Home.jsx
@@ -1,5 +1,9 @@
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <div>
+      Home <h1>Raju</h1> <h2>Tamang</h2>
+    </div>
+  )
 }
 
 export default Home
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import sass from 'sass'
+import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        implementation: sass,
+      },
+    },
+  },
 })
