a939e3adbcd66b9f2d7caa0617326492d672b3b6
rajutamang1011 <rajutamang1011@gmail.com>
2024-01-17 16:18:22 +0545

    hero_complete

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,19 +1,32 @@
+import { MdKeyboardArrowRight } from 'react-icons/md'
 import styles from './hero.module.scss'
-
 const HeroSection = () => {
   return (
     <section className={styles.heroSection}>
       <div className="container">
         <div className="row">
-          <div className={styles.flex_col}>
+          <div className={`${styles.flex_col} ${styles.left}`}>
             <h1>
-              The CLIP is <span>we will meet all your academic needs.</span>
+              <span>The CLIP is</span> <br />
+              We will meet all your academic needs.
             </h1>
-            <div className="boxWrap">
-              <div className="box">1</div>
-              <div className="box">1</div>
-              <div className="box">1</div>
-              <div className="box">1</div>
+            <div className={styles.boxWrap}>
+              <div className={styles.box}>
+                <p>All text book for lectures</p>
+                <h3>e-book</h3>
+              </div>
+              <div className={styles.box}>
+                <p>Various classes related</p>
+                <h3>Material</h3>
+              </div>
+              <div className={styles.box}>
+                <p>For non face to face classes</p>
+                <h3>e-learning</h3>
+              </div>
+              <div className={styles.box}>
+                <p>For report generation</p>
+                <h3>HMI System</h3>
+              </div>
             </div>
             <p>
               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex
@@ -21,8 +34,14 @@ const HeroSection = () => {
               Consequatur, ullam! Officia quas architecto, inventore fugiat
               repellendus deserunt quod dolorum quidem corrupti aperiam!
             </p>
+            <div className={styles.buttonWrapper}>
+              <a href="#">
+                View brochure <MdKeyboardArrowRight className={styles.icon} />
+              </a>
+            </div>
           </div>
-          <div className={styles.flex_col}>
+          <div className={`${styles.flex_col} ${styles.right}`}>
+            <div className={styles.circle}></div>
             <img src="images/hero.png" alt="hero_image" />
           </div>
         </div>
